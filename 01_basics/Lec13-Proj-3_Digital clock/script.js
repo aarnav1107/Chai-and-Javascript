2230a8f2f8e4a591694bcc3756c192ae1bdfbf66
Aarnav Singh <aarnavsingh.11@gmail.com>
2024-05-09 02:01:01 +0530

    Add files via upload

---
+++ 01_basics/Lec13-Proj-3_Digital clock/script.js
@@ -0,0 +1,11 @@
+const center=  document.querySelector('.center')
+let date = new Date();
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock')
+
+setInterval(function () {
+  let date = new Date();
+  // console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
