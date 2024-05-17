e4384874f93d22a8fc232c2bc14787eb0b6699d6
Aarnav Singh <aarnavsingh.11@gmail.com>
2024-05-17 22:12:11 +0530

    Add files via upload

--- 01_basics/Lec25 myclasses/s.js
+++ 01_basics/Lec25 myclasses/s.js
@@ -15,28 +15,40 @@ class User {
 }
 
 const chai = new User("chai", "chai@gmail.com", "123")
-console.log(chai.encryptPassword());
-console.log(chai.changeUsername());
-console.log(chai.username);
+ console.log(chai.encryptPassword());
+ console.log(chai.changeUsername());
+// console.log(chai.username);
+
 console.log(chai.email);
+console.log(chai);
 
 // behind the scene
 
-function User(username, email, password){
-    this.username = username;
-    this.email = email;
-    this.password = password
+// function User(username, email, password){
+//     this.username = username;
+//     this.email = email;
+//     this.password = password
+// }
+
+// User.prototype.encryptPassword = function(){
+//     return `${this.password}abc`
+// }
+// User.prototype.changeUsername = function(){
+//     return `${this.username.toUpperCase()}`
+// }
+
+
+// const tea = new User("tea", "tea@gmail.com", "123")
+
+// console.log(tea.encryptPassword());
+// console.log(tea.changeUsername());
+
+
+let x= 10;
+if(x==10){
+    let x = 20;
+    console.log(x);
 }
-
-User.prototype.encryptPassword = function(){
-    return `${this.password}abc`
-}
-User.prototype.changeUsername = function(){
-    return `${this.username.toUpperCase()}`
-}
-
-
-const tea = new User("tea", "tea@gmail.com", "123")
-
-console.log(tea.encryptPassword());
-console.log(tea.changeUsername());
+else{
+    console.log(x);
+}   
